9a1f18d2fe154c11ec735ff0169be42298fc106c
dieromeo <diegoromeo99@icloud.com>
2024-02-05 15:41:51 +0100

    feat: old text to create and update

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -25,31 +25,31 @@
             @csrf
             <div class="mb-3 col-6">
                 <label for="nome" class="form-label">Nome Fumetto</label>
-                <input type="text" class="form-control" id="nome" name="title">
+                <input type="text" class="form-control" id="nome" name="title" value="{{ old('title') }}">
             </div>
             <div class="mb-3 col-6">
                 <label for="serie" class="form-label">Serie Fumetto</label>
-                <input type="text" class="form-control" id="serie" name="series">
+                <input type="text" class="form-control" id="serie" name="series" value="{{ old('series') }}">
             </div>
             <div class="mb-3 col-3">
                 <label for="tipo" class="form-label">Tipo Fumetto</label>
-                <input type="text" class="form-control" id="tipo" name="type">
+                <input type="text" class="form-control" id="tipo" name="type" value="{{ old('type') }}">
             </div>
             <div class="mb-3 col-9">
                 <label for="link" class="form-label">Link Copertina</label>
-                <input type="text" class="form-control" id="link" name="thumb">
+                <input type="text" class="form-control" id="link" name="thumb" value="{{ old('thumb') }}">
             </div>
             <div class="mb-3 col-4">
                 <label for="prezzo" class="form-label">Prezzo</label>
-                <input type="number" class="form-control" id="prezzo" name="price">
+                <input type="number" class="form-control" id="prezzo" name="price" value="{{ old('price') }}">
             </div>
             <div class="mb-3 col-4">
                 <label for="data" class="form-label">Data</label>
-                <input type="date" class="form-control" id="data" name="sale_date">
+                <input type="date" class="form-control" id="data" name="sale_date" value="{{ old('sale_date') }}">
             </div>
             <div class="mb-3">
                 <label for="descrizione" class="form-label">Descrizione fumetto</label>
-                <textarea name="description" id="descrizione" cols="30" rows="5" class="form-control"></textarea>
+                <textarea name="description" id="descrizione" cols="30" rows="5" class="form-control">{{ old('description') }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary ">Submit</button>
         </form>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -25,31 +25,37 @@
             @method('PUT')
             <div class="mb-3 col-6">
                 <label for="nome" class="form-label">Nome Fumetto</label>
-                <input type="text" class="form-control" id="nome" name="title" value="{{ $comic->title }}">
+                <input type="text" class="form-control" id="nome" name="title"
+                    value="{{ old('title', $comic->title) }}">
             </div>
             <div class="mb-3 col-6">
                 <label for="serie" class="form-label">Serie Fumetto</label>
-                <input type="text" class="form-control" id="serie" name="series" value="{{ $comic->series }}">
+                <input type="text" class="form-control" id="serie" name="series"
+                    value="{{ old('series', $comic->series) }}">
             </div>
             <div class="mb-3 col-3">
                 <label for="tipo" class="form-label">Tipo Fumetto</label>
-                <input type="text" class="form-control" id="tipo" name="type" value="{{ $comic->type }}">
+                <input type="text" class="form-control" id="tipo" name="type"
+                    value="{{ old('type', $comic->type) }}">
             </div>
             <div class="mb-3 col-9">
                 <label for="link" class="form-label">Link Copertina</label>
-                <input type="text" class="form-control" id="link" name="thumb" value="{{ $comic->thumb }}">
+                <input type="text" class="form-control" id="link" name="thumb"
+                    value="{{ old('thumb', $comic->thumb) }}">
             </div>
             <div class="mb-3 col-4">
                 <label for="prezzo" class="form-label">Prezzo</label>
-                <input type="number" class="form-control" id="prezzo" name="price" value="{{ $comic->price }}">
+                <input type="number" class="form-control" id="prezzo" name="price"
+                    value="{{ old('price', $comic->price) }}">
             </div>
             <div class="mb-3 col-4">
                 <label for="data" class="form-label">Data</label>
-                <input type="date" class="form-control" id="data" name="sale_date" value="{{ $comic->sale_date }}">
+                <input type="date" class="form-control" id="data" name="sale_date"
+                    value="{{ old('sale_date', $comic->sale_date) }}">
             </div>
             <div class="mb-3">
                 <label for="descrizione" class="form-label">Descrizione fumetto</label>
-                <textarea name="description" id="descrizione" cols="30" rows="5" class="form-control">{{ $comic->description }}</textarea>
+                <textarea name="description" id="descrizione" cols="30" rows="5" class="form-control">{{ old('description', $comic->description) }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary ">Submit</button>
         </form>
