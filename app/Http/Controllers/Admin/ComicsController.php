6f0223db1447cbb09e60c02c22bc28c9fbea15f7
dieromeo <diegoromeo99@icloud.com>
2024-02-05 15:31:35 +0100

    feat: custom messages

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -6,12 +6,36 @@ use App\Http\Controllers\Controller;
 use App\Models\Comic;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
 class ComicsController extends Controller
 {
-    public function validator(Request $request)
+    private function validator($data)
     {
+        $validator = Validator::make($data, [
+            'title' => 'required|max:100',
+            'description' => 'min:20|nullable',
+            'thumb' => 'image|nullable',
+            'price' => 'required|integer',
+            'series' => 'required|min:5',
+            'type' => ['required', Rule::in(['comic book', 'graphic novel']),],
+            'sale_date' => 'date|nullable'
+        ], [
+            'title.required' => 'Il titolo è obbligatorio',
+            'title.max' => 'Il titolo deve essere al massimo di 30 caratteri',
+            'description.min' => 'La descrizione è troppo corta. Min. 20 caratteri',
+            'thumb' => 'l\'immagine deve essere di un tipo valido',
+            'price.required' => 'Inserire un prezzo',
+            'price.integer' => 'Il prezzo deve essere un numero',
+            'series.required' => 'Inserire la serie',
+            'series.min' => 'La serie deve essere più lunga',
+            'type.required' => 'Inserire il tipo',
+            'sale_date.required' => 'Inserire una data valida',
+
+        ])->validate();
+
+        return $validator;
     }
 
 
@@ -37,24 +61,10 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'title' => 'required|max:100',
-            'description' => 'min:20|nullable',
-            'thumb' => 'image|nullable',
-            'price' => 'required|integer',
-            'series' => 'required|min:5',
-            'type' => ['required', Rule::in(['comic book', 'graphic novel']),],
-            'sale_date' => 'date|nullable'
-        ]);
-
+        $data = $this->validator($request->all());
         $comic = new Comic();
-        $comic->title = $validated['title'];
-        $comic->description = $validated['description'];
-        $comic->thumb = $validated['thumb'];
-        $comic->price = $validated['price'];
-        $comic->series = $validated['series'];
-        $comic->sale_date = $validated['sale_date'];
-        $comic->type = $validated['type'];
+
+        $comic->fill($data);
         $comic->save();
 
         return redirect()->route('comics.index');
@@ -81,18 +91,8 @@ class ComicsController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
-
-        $validated = $request->validate([
-            'title' => 'required|max:100',
-            'description' => 'min:20|nullable',
-            'thumb' => 'image|nullable',
-            'price' => 'required|integer',
-            'series' => 'required|min:5',
-            'type' => ['required', Rule::in(['comic book', 'graphic novel']),],
-            'sale_date' => 'date|nullable'
-        ]);
-
-        $comic->update($validated);
+        $data = $this->validator($request->all());
+        $comic->update($data);
         return redirect()->route('comics.index');
     }
 
